1d67c603ce65ed729fa34d3776ccfa558abea8e9
JozefinaNikolova <jozi5@abv.bg>
2017-04-19 20:43:22 +0300

    Added seed method with admin role

--- Auction/Auction.Data/Migrations/Configuration.cs
+++ Auction/Auction.Data/Migrations/Configuration.cs
@@ -1,32 +1,97 @@
 namespace Auction.Data.Migrations
 {
     using System;
-    using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
+    using Data;
+    using Models;
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using Microsoft.AspNet.Identity;
+    using System.IO;
 
     public sealed class Configuration : DbMigrationsConfiguration<Auction.Data.AuctionContext>
     {
+        private const string DefaultAdminEmail = "admin@admin.com";
+        private const string DefaultAdminFullName = "Admin";
+        private const string DefaultAdminPhoneNumber = "07001700";
+        private const string DefaultAdminPassword = "123456";
+        private const string AdministratorRoleName = "Administrator";
+
         public Configuration()
         {
             AutomaticMigrationsEnabled = true;
             AutomaticMigrationDataLossAllowed = true;
         }
 
-        protected override void Seed(Auction.Data.AuctionContext context)
+        protected override void Seed(AuctionContext context)
         {
-            //  This method will be called after migrating to the latest version.
+            if(!context.Users.Any(x => x.Email == DefaultAdminEmail))
+            {
+                var adminEmail = DefaultAdminEmail;
+                var adminUsername = adminEmail;
+                var adminFullName = DefaultAdminFullName;
+                var adminPhoneNumber = DefaultAdminPhoneNumber;
+                var adminPassword = DefaultAdminPassword;
+                var adminRole = AdministratorRoleName;
 
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
+                CreateAdminUser(context, adminEmail, adminUsername, adminFullName, adminPhoneNumber, adminPassword, adminRole);
+            }
+
+            if (!context.Categories.Any())
+            {
+                AddCategories(context);
+            }
+        }
+
+        private void CreateAdminUser(AuctionContext context, string adminEmail, string adminUsername, string adminFullName, string adminPhoneNumber, string adminPassword, string adminRole)
+        {
+            //Create admin role
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            roleManager.Create(new IdentityRole(AdministratorRoleName));
+
+            var adminUser = new User
+            {
+                UserName = adminUsername,
+                Email = adminEmail,
+                FullName = adminFullName,
+                PhoneNumber = adminPhoneNumber
+            };
+
+            var userStore = new UserStore<User>(context);
+            var userManager = new UserManager<User>(userStore);
+
+            userManager.PasswordValidator = new PasswordValidator
+            {
+                RequiredLength = 6,
+                RequireNonLetterOrDigit = false,
+                RequireDigit = false,
+                RequireLowercase = false,
+                RequireUppercase = false
+            };
+
+            var userCreateResult = userManager.Create(adminUser, adminPassword);
+            if (!userCreateResult.Succeeded)
+            {
+                throw new Exception(string.Join("; ", userCreateResult.Errors));
+            }
+
+            var addAdminRoleResult = userManager.AddToRole(adminUser.Id, adminRole);
+            if (!addAdminRoleResult.Succeeded)
+            {
+                throw new Exception(string.Join("; ", addAdminRoleResult.Errors));
+            }
+
+            context.SaveChanges();
+        }
+
+        private void AddCategories(AuctionContext context)
+        {
+            context.Categories.Add(new Category { Name = "Home" });
+            context.Categories.Add(new Category { Name = "Office" });
+            context.Categories.Add(new Category { Name = "Clothes" });
+            context.Categories.Add(new Category { Name = "Technology" });
+
+            context.SaveChanges();
         }
     }
 }
